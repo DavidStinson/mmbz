d477fd46598656022b04110aba31b41abed7a11b
teradaian <teradaian@gmail.com>
2023-02-10 12:27:40 -0800

    finish first levelup with hover in votemanager

--- src/components/VoteManager/VoteManager.tsx
+++ src/components/VoteManager/VoteManager.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import bean from '../../assets/icons/bean.png'
 import noBean from '../../assets/icons/noBean.png'
 import { VoteHandlerProps } from '../../types/forms'
 
+
 const VoteManager = ({ profile, handleVote }: VoteHandlerProps) => {
+  const [hover, setHover] = useState<string | null>(null)
+
   const voteCount = profile.votesReceived.length
   const total: number = profile.votesReceived.reduce((sum: number, v: { value: number }) => {
     return sum + v.value
@@ -15,6 +19,16 @@ const VoteManager = ({ profile, handleVote }: VoteHandlerProps) => {
     handleVote({ value: newValue, profileId: profile.id })
   }
 
+  const handleHover = (evt: React.MouseEvent<HTMLImageElement>) => {
+    const img = evt.currentTarget
+
+    if(evt.type === 'mouseover'){
+      setHover(img.id)
+    } else if (evt.type === 'mouseleave'){
+      setHover(null)
+    }
+  }
+
   return ( 
     <section>
       {Array.from({ length: 5 }, ((el: never, idx: number) => (
@@ -22,7 +36,9 @@ const VoteManager = ({ profile, handleVote }: VoteHandlerProps) => {
           id={idx.toString()}
           key={idx.toString()}
           onClick={handleClick}
-          src={idx < rating ? bean : noBean} alt="Bean Symbol"
+          onMouseOver={handleHover}
+          onMouseLeave={handleHover}
+          src={idx <= (hover ?? rating -1) ? bean : noBean} alt="Bean Symbol"
         />
       )))}
     </section>
